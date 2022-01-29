c84b4818d0e645610b18c8b254d662af3e530e78
ig-dev-eu <98580918+ig-dev-eu@users.noreply.github.com>
2022-01-29 21:29:14 +0100

    Changes in view_players.php
    All table related style isn't defined here anymore, but in the css file linked.

--- view_players.php
+++ view_players.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,20 +7,25 @@
     <link rel="stylesheet" href="functionalpage.css">
     <title>Document</title>
     <style>
-        table {
-            font-size: 1.7rem;
-            color: white;
-            height: 60%;
+        body {
+            justify-content: start;
         }
     </style>
 </head>
 
 <body>
+    <h1>All Basketball Players</h1>
 
     <?php
         $handler = fopen("Basket1.txt", "r");
 
         echo "<table>";
+        
+        echo "<tr>";
+        echo "<th>"."Name"."</th>";
+        echo "<th>"."Surname"."</th>";
+        echo "<th>"."Height"."</th>";
+        echo "</tr>";
 
         while (!feof($handler)){
             $content = fgets($handler);
